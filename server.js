4540f8176b7b9fd5ace4124729c69c6856ab3ec8
mmiiles <milesconrad@pm.me>
2021-03-14 01:32:27 -0500

    implemented interests back end

--- server.js
+++ server.js
@@ -10,19 +10,39 @@ const interestUsers = {};
 io.on('connection', (socket) => {
 
   socket.on('join', (interest) => {
-    queuedUsers[socket.id] = socket;
-    if (Object.keys(queuedUsers).length >= 2) {
-      const userids = Object.keys(queuedUsers);
-      const user1 = queuedUsers[userids[0]];
-      const user2 = queuedUsers[userids[1]];
-      delete queuedUsers[userids[0]];
-      delete queuedUsers[userids[1]];
+    
+    if (interest) {
+      if (Object.keys(interestUsers).length >= 1 && interestUsers[interest]) {
+        const user1 = interestUsers[interest];
+        const user2 = socket;
+        delete interestUsers[interest];
 
-      user1.partner = user2;
-      user2.partner = user1;
-      user1.emit('partnered');
-      user2.emit('partnered');
+        user1.partner = user2;
+        user2.partner = user1;
+        user1.emit('partnered');
+        user2.emit('partnered');
+      }
+      else {
+        interestUsers[interest] = socket;
+      };
     };
+
+    if (!interest) {
+      queuedUsers[socket.id] = socket;
+      if (Object.keys(queuedUsers).length >= 2) {
+        const userids = Object.keys(queuedUsers);
+        const user1 = queuedUsers[userids[0]];
+        const user2 = queuedUsers[userids[1]];
+        delete queuedUsers[userids[0]];
+        delete queuedUsers[userids[1]];
+
+        user1.partner = user2;
+        user2.partner = user1;
+        user1.emit('partnered');
+        user2.emit('partnered');
+      };
+    };
+
   });
 
   socket.on('send', (length) => {
