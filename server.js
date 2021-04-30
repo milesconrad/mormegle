00997a0bdbec77e01d34bd481acbba320aa77d68
mmiiles <milesconrad@pm.me>
2021-04-30 15:48:38 -0400

    added comments

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ io.on('connection', (socket) => {
 
   socket.on('join', (interest) => {
     interest = interest.toLowerCase();
-    
     if (interest) {
+
+      // if there is another interested user in the waiting room and they share
+      // interests, remove them from the waiting room and partner them
       if (Object.keys(interestUsers).length > 0 && interestUsers[interest]) {
         const user1 = interestUsers[interest];
         const user2 = socket;
@@ -23,37 +25,50 @@ io.on('connection', (socket) => {
         user1.emit('partnered');
         user2.emit('partnered');
       }
+
+      // if there are not enough users in the waiting room or none of them share
+      // interests, add the current user to the waiting room
       else {
         interestUsers[interest] = socket;
       };
     };
 
     if (!interest) {
-      queuedUsers[socket.id] = socket;
-      if (Object.keys(queuedUsers).length >= 2) {
+
+      // if there is another user in the waiting room, remove them from the waiting
+      // room and partner them
+      if (Object.keys(queuedUsers).length > 0) {
         const userids = Object.keys(queuedUsers);
         const user1 = queuedUsers[userids[0]];
-        const user2 = queuedUsers[userids[1]];
+        const user2 = socket;
         delete queuedUsers[userids[0]];
-        delete queuedUsers[userids[1]];
 
         user1.partner = user2;
         user2.partner = user1;
         user1.emit('partnered');
         user2.emit('partnered');
+      }
+
+      // if there are no other users in the waiting room, add the current user to
+      // the waiting room
+      else {
+        queuedUsers[socket.id] = socket;
       };
     };
-
   });
 
-  socket.on('send', (length) => {
-    socket.partner.emit('send', length);
+  socket.on('send', (duration) => {
+    socket.partner.emit('send', duration);
   });
 
   socket.on('disconnect', () => {
+
+    // if the user disconnected, let their partner know
     if (socket.partner) {
       socket.partner.emit('abandoned');
     }
+    
+    // if the user disconnected while in the waiting room, remove them from the waiting room
     if (queuedUsers[socket.id]) {
       delete queuedUsers[socket.id];
     }
